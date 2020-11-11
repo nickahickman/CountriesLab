6e587346b2e22bd48526780e0749472fab148cf6
nickahickman <nickahickman@gmail.com>
2020-11-11 08:53:03 -0500

    added flag display feature and improved text visibility

--- scripts/country.js
+++ scripts/country.js
@@ -1,17 +1,54 @@
 class Country {
-    constructor(name, lang, greeting, colors) {
+    constructor(name, lang, greeting, colors, flag) {
         this.name = name;
         this.lang = lang;
         this.greeting = greeting;
         this.colors = colors;
+        this.flag = flag;
+    }
+    Display() {
+        //change text
+        if (this.name === "Egypt" || this.name === "USA")
+        {
+            let allh2 = document.querySelectorAll("h2");
+            for (let i = 0; i < allh2.length; i++)
+            {
+                allh2[i].style.color = "white";
+            }
+        }
+        else {
+            let allh2 = document.querySelectorAll("h2");
+            for (let i = 0; i < allh2.length; i++)
+            {
+                allh2[i].style.color = "black";
+            }
+        }
+
+        document.getElementById("CountryName").innerText = this.name;
+        document.getElementById("OfficialLanguage").innerText = this.lang;
+        document.getElementById("HelloWorld").innerText = this.greeting;
+
+        //change css
+        document.getElementById("Color1").style.backgroundColor = this.colors[0];
+        document.getElementById("Color2").style.backgroundColor = this.colors[1];
+
+        if (this.colors.length > 2) {
+            document.getElementById("Color3").style.backgroundColor = this.colors[2];
+        }
+        else {
+            document.getElementById("Color3").style.backgroundColor = this.colors[1];
+        }
+
+        //change flag
+        document.getElementById("flag").src = this.flag;
     }
 }
 
-let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["#b22234", "white", "#3c3b6e"]);
-let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
-let japan = new Country("Japan", "Japanese", "Kon'nichiwa sekai", ["red", "white"]);
-let egypt = new Country("Egypt", "Arabic", "Sup", ["red", "white", "black"]);
-let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"]);
+let usa = new Country("USA", "Murican", "USA! USA! USA! USA!", ["#b22234", "#fff", "#3c3b6e"], 'img/usaflag.png');
+let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["#ce1126", "#fff", "#006847"], 'img/mexicoflag.png');
+let japan = new Country("Japan", "Japanese", "Kon'nichiwa sekai", ["#be0029", "#fff"], 'img/japanflag.png');
+let egypt = new Country("Egypt", "Arabic", "Marhabaan bialealam", ["#ce1126", "#fff", "#000"], 'img/egyptflag.png');
+let algeria = new Country("Algeria", "Arabic and Berber", "Marhabaan bialealam", ["#007a33", "#fff", "#c8102e"], 'img/algeriaflag.png');
 
 let countries = [usa, mexico, japan, egypt, algeria];
 
@@ -22,34 +59,22 @@ function SwitchCountry() {
     if (input === "USA") {
         //set country to usa 
         country = usa;
+        country.Display();
     }
     else if (input === "Mexico") {
         country = mexico;
+        country.Display();
     }
     else if (input === "Japan") {
         country = japan;
+        country.Display();
     }
     else if (input === "Egypt") {
         country = egypt;
+        country.Display();
     }
     else if (input === "Algeria") {
         country = algeria;
+        country.Display();
     }
-
-    //change text
-    document.getElementById("CountryName").innerText = country.name;
-    document.getElementById("OfficialLanguage").innerText = country.lang;
-    document.getElementById("HelloWorld").innerText = country.greeting;
-
-    //change css
-    document.getElementById("Color1").style.backgroundColor = country.colors[0];
-    document.getElementById("Color2").style.backgroundColor = country.colors[1];
-
-    if (country.colors.length > 2) {
-        document.getElementById("Color3").style.backgroundColor = country.colors[2];
-    }
-    else {
-        document.getElementById("Color3").style.backgroundColor = country.colors[1];
-    }
-    
 }
